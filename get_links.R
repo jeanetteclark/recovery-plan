37a3d49587088cb72b61ef7825d3658651cd8e81
Jeanette Clark <jclark@nceas.ucsb.edu>
2022-06-24 14:53:03 -0700

    use relative paths

--- get_links.R
+++ get_links.R
@@ -1,13 +1,13 @@
 library(rvest)
 
-links <- "~/recovery-plan/recovery-plan-data.html" %>% 
+links <- "recovery-plan-data.html" %>% 
     read_html() %>% 
     html_nodes("a") %>% 
     html_attrs() %>% 
     grep("infobar|mailto|#|^primarydata", ., invert = T, value = T)
 
 download_files <- function(link){
-    download.file(link, paste("~/recovery-plan/", basename(link)))
+    download.file(link, basename(link))
 }
 
 lapply(links, download_files)
